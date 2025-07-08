e5c90e853826ff9ba8b11412f6abf22b310bbf17
04Shubhampatil <04Shubhampatil>
2025-07-08 18:09:08 +0530

    build(frontend): add tailwindcss integration
    - Add tailwindcss import to index.css
    - Include tailwindcss plugin in vite config
    - Update App.tsx to use tailwind classes for styling
    
    Signed-off-by: 04Shubhampatil <04Shubhampatil>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App:React.FC = () => {
   return (
-    <div>App</div>
+    <div className='bg-zinc-400'>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos perspiciatis aliquid nemo error enim officiis quasi. Amet obcaecati, similique quibusdam qui beatae explicabo, eum expedita voluptatem, error est maiores quaerat.
+    </div>
   )
 }
 
